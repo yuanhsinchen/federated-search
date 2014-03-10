235a5c52f76bcf2c0686560514cfc7fcccdfcbe4
Yuan-Hsin Chen <yuanlmm@gmail.com>
2014-03-10 18:34:51 -0400

    cleaned index.py

--- index.py
+++ index.py
@@ -19,18 +19,6 @@ urls = (
 
 app = web.application(urls, globals())
 
-class Render(QWebPage):  
-  def __init__(self, url):  
-    self.app = QApplication(sys.argv)  
-    QWebPage.__init__(self)  
-    self.loadFinished.connect(self._loadFinished)  
-    self.mainFrame().load(QUrl(url))  
-    self.app.exec_()  
-  
-  def _loadFinished(self, result):  
-    self.frame = self.mainFrame()  
-    self.app.quit()
-
 class index:
   def __init__(self):
     self.render = web.template.render('template')
@@ -48,30 +36,15 @@ class result:
   def GET(self, name=None):
     i = web.input(query=None)
     query = i.query.replace(" ", "+")
-    s = "http://citeseerx.ist.psu.edu/search?q=" + query + "&submit=Search&sort=rlv&t=doc&feed=rss"
-    response = urllib2.urlopen(s)
-    rss = response.read()
-    citeseers_rss = feedparser.parse(s)
-    #for entr in citeseers_rss.entries:
-      #print entr.title
 
+    #query to CiteSeerx
+    s = "http://citeseerx.ist.psu.edu/search?q=" + query + "&submit=Search&sort=rlv&t=doc&feed=rss"
+    citeseers_rss = feedparser.parse(s)
+
+    #query to CiteSeerx Author
     s = "http://citeseerx.ist.psu.edu/search?q=" + query + "&submit=Search&uauth=1&sort=ndocs&t=auth"
-    #html = urllib2.urlopen(s).read()
-    #r = soup.findAll("div", { "class" : "result" })
-    #rrender = Render(s)
-    #html = rrender.frame.toHtml()
-    #f = open(i.query + '.html', 'w')
-    #f.write(html)
-    #f.close()
     doc = lxml.html.parse(s)
     r = doc.xpath('//div/h3/a')
-    #for attr in r:
-    #  if attr == 'result':
-    #    author = doc.xpath('//div/div/h3/a/@href')
-    #    print author
-    #for ele in r:
-    #  print ele.attrib
-    #  print ele.text
     return self.render.result(citeseers_rss, r)
 
 if __name__ == "__main__":
