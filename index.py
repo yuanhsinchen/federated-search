26fbdffd6f076acbf86e403dab182a3d3b633792
Yuan-Hsin Chen <yuanlmm@gmail.com>
2014-04-14 01:56:37 -0400

    process html returned from CiteseerX instead of RSS

--- index.py
+++ index.py
@@ -39,6 +39,7 @@ class result:
     query = i.query.replace(" ", "+")
 
     #query to CiteSeerx
+    """
     s = "http://citeseerx.ist.psu.edu/search?q=" + query + "&submit=Search&sort=rlv&t=doc&feed=rss"
     rss = feedparser.parse(s)
     for entr in rss.entries:
@@ -46,7 +47,24 @@ class result:
         entr.title = entr.title.replace("</em>", " ")
         entr.description = entr.description.replace("<em>", " ")
         entr.description = entr.description.replace("</em>", " ")
-
+    """
+    s = "http://citeseerx.ist.psu.edu/search?q=" + query + "&submit=Search&sort=rlv&t=doc"
+    citeseerx = lxml.html.parse(s)
+    result_div = citeseerx.xpath("//div[@class='result']")
+    citeseerx_result = []
+    for div in result_div:
+        info = {}
+        info['href'] = div.xpath("h3/a/@href")
+        #title = div.xpath("h3/a//text()").strip().replace('<em>', '').replace('</em>', '').replace('\n', '')
+        title = div.xpath("h3/a//text()")
+        title = [s.strip() for s in title]
+        title = ' '.join(title)
+        info['title'] = title
+        author = ''.join(div.xpath("div[@class='pubinfo']/span[@class='authors']/text()"))
+        info['author'] = author.replace('\n', '').replace('by', '').strip().split(',')
+        citedby = ''.join(div.xpath("div[@class='pubextras']/a[@class='citation remove']/text()"))
+        citeseerx_result.append(info)
+    print citeseerx_result
     #query to CiteSeerx Author
     #s = "http://citeseerx.ist.psu.edu/search?q=" + query + "&submit=Search&uauth=1&sort=ndocs&t=auth"
     #query to CSSeer
@@ -57,7 +75,7 @@ class result:
         info = {}
         au_url = 'http://csseer.ist.psu.edu/experts/'
         info['href'] = au_url + ''.join((node.xpath("ul/li/a/@href")))
-        info['name'] = ''.join(node.xpath("ul/li/a/text()"))
+        info['author'] = ''.join(node.xpath("ul/li/a/text()"))
         #s = ''.join(node.xpath("table[@class='authInfo']/tr[contains(.,'Variations')]/td[2]/text()"))
         #info['Variations'] = s
         info['Affiliations'] = ''.join(node.xpath("ul/li[2]/text()"))
@@ -66,7 +84,7 @@ class result:
         #s = ''.join(node.xpath("table[@class='authInfo']/tr[contains(.,'Homepage')]/td[2]/a/@href"))
         #info['Homepage'] = s
         html.append(info)
-    return self.render.result(rss, html)
+    #return self.render.result(citeseerx, html)
 
 class css:
     def GET(self): raise web.seeother("/static/inputcss.css")
